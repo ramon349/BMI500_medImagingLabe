6c96c4e204ad4b6cbd21aff2156572d232e58bce
ramon <rlcorre@emory.edu>
2020-10-21 18:35:32 -0400

    leaky plotting

---
+++ leaky_plot_perf.py
@@ -0,0 +1,97 @@
+import re
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from leaky_model_builder import build_model 
+from preprocessing import process_path,prepare_for_training
+
+def show_batch( iterator):
+    plt.figure(figsize=(10,10))
+    for n,(image,label) in enumerate( iterator.take(25)):
+        ax = plt.subplot(5,5,n+1)
+        plt.imshow(image)
+        if label:
+            plt.title("PNEUMONIA")
+        else:
+            plt.title("NORMAL")
+        plt.axis("off")
+    plt.show()
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+GCS_PATH = "/labs/colab/BMI500-Fall2020/"
+
+BATCH_SIZE = 16 * 10
+IMAGE_SIZE = [180, 180]
+EPOCHS = 25 
+train_dataset_path = os.path.abspath(os.path.join(GCS_PATH,'chest_xray/train/*/*'))
+print(train_dataset_path)
+test_dataset_path = os.path.abspath(os.path.join(GCS_PATH,'chest_xray/val/*/*'))
+filenames = tf.io.gfile.glob(train_dataset_path) 
+filenames.extend(tf.io.gfile.glob(test_dataset_path) ) 
+
+COUNT_NORMAL = len([filename for filename in train_filenames if "NORMAL" in filename])
+
+
+val_list_ds = tf.data.Dataset.from_tensor_slices(val_filenames)
+
+
+val_ds = val_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.batch(BATCH_SIZE)
+
+
+
+
+VAL_IMG_COUNT = tf.data.experimental.cardinality(val_list_ds).numpy()
+print("Validating images count: " + str(VAL_IMG_COUNT))
+
+
+weight_for_0 = (1 / COUNT_NORMAL)*(TRAIN_IMG_COUNT)/2.0 
+weight_for_1 = (1 / COUNT_PNEUMONIA)*(TRAIN_IMG_COUNT)/2.0
+
+class_weight = {0: weight_for_0, 1: weight_for_1}
+
+
+
+model = build_model(IMAGE_SIZE)
+
+METRICS = [
+    'accuracy',
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall'),
+    tf.keras.metrics.AUC(name='auc', curve='ROC'),
+    tf.keras.metrics.AUC(name='pr', curve='PR')
+]
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=METRICS
+)
+
+model.load_weights("/labs/colab/BMI500-Fall2020/BMI500_jjeong/leaky_model.cpt.data-00000-of-00001")
+
+import pdb 
+pdb.set_trace()
+history = model.fit(
+    train_ds,
+    steps_per_epoch=TRAIN_IMG_COUNT // BATCH_SIZE,
+    epochs=25,
+    validation_data=val_ds,
+    validation_steps=VAL_IMG_COUNT // BATCH_SIZE,
+    class_weight=class_weight,
+    callbacks=[cp_callback]
+)
+
+fig, ax = plt.subplots(1, 4, figsize=(20, 3))
+ax = ax.ravel()
+for i, met in enumerate(['precision', 'recall', 'accuracy', 'auc', 'pr', 'loss']):
+    ax[i].plot(history.history[met])
+    ax[i].plot(history.history['val_' + met])
+    ax[i].set_title('Model {}'.format(met))
+    ax[i].set_xlabel('epochs')
+    ax[i].set_ylabel(met)
+    ax[i].legend(['train', 'val'])
+plt.savefig('leaky.png')
